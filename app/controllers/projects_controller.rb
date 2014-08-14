0ce604e2435636844edb8b6450aa6e7f33a87658
Joel Graycar <jgraycar@berkeley.edu>
2014-08-14 16:44:18 -0700

    changed redirect after project creation to index page anchor

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,7 +13,7 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
  
     if @project.save
-      redirect_to @project
+      redirect_to projects_path + "##{@project.slug}"
     else
       render 'new'
     end
