7c610893f38185ada98afb8a9b1a000b83310c3a
Joel Graycar <jgraycar@berkeley.edu>
2014-08-21 04:23:24 -0700

    removed signup feature from users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #:registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # +login+ is used to authenticate users when logging in.
